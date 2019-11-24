039cd88dd84ca3fd1418efd505ebc6e9731b1b55
今井千華 <imaichika@imaichikanoMacBook-ea.local>
2019-11-24 22:57:02 +0900

    AWS lambda用のindex.jsを編集

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const request = require('request-promise');
 exports.handler = async (event) => {
   
+  // バスのルートと停留所をDialogFlowの変数から取り出す
   const body = JSON.parse(event.body);
     const busline = body.queryResult.parameters.busline;
-    console.log(busline);
     const busstop = body.queryResult.parameters.busstop;
-    console.log(busstop);
+  // 返信用のテキストを準備
     var speechText = '';
-
+  // 返信用テキストを取得
   await regist(busline, busstop)
       .then(val => {
           speechText = val;
   });
-  
+  // エラーが起きたときの処理
   if (speechText === "miss") {
       speechText = "エラーが起きています、もう一度打ち直してね！";
   }
   
-  
+  // Dialogflowへのリクエストを生成
   return new Promise(function(resolve) {
       const responseJson = JSON.stringify({
         "fulfillmentText": "",
@@ -37,10 +37,10 @@ exports.handler = async (event) => {
       resolve(response);
     });
 };
-
+// gasへ投げるURLを取得してgasにrequestで投げる
 const regist = (busline, busstop) => {
       let url = process.env.Gas + "?busline=" + busline + "&busstop=" + busstop;
+      // 2バイト文字を扱うためエンコード処理
       url = encodeURI(url);
-      console.log(url);
       return request(url);
   };
